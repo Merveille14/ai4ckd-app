23b55ced64815ec1699bff2fa09c87b72cada811
Pretty <tchepretty@gmail.com>
2025-03-31 08:32:12 +0100

    Migrations/models/controller

---
+++ backend/database/migrations/2025_03_31_070709_alertes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alertes', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->text('message');
+            $table->foreignId('medecin_id')->constrained('medecins')->onDelete('cascade');
+            $table->boolean('envoyee')->default(false);
+            $table->timestamp('date_envoi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alertes');
+    }
+};
