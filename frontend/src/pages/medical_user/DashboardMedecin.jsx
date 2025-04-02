9dd7b0c2c59e10de69a7e323268356b5dd22a0ea
kayodeb <kayodebatcho@gmail.com>
2025-04-02 15:21:44 +0000

    sidebarMedical

--- frontend/src/pages/medical_user/DashboardMedecin.jsx
+++ frontend/src/pages/medical_user/DashboardMedecin.jsx
@@ -3,7 +3,7 @@ import Chart from 'chart.js/auto';
 import "../../App.css";
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Home, Users, ClipboardList, BarChart, LogOut, Bell, Mail, Settings, CheckCircle, Clock, UserCheck, AlertTriangle, TrendingUp } from 'lucide-react';
-
+import SiderbarMedical from '@/components/sidebarMedical';
 const DashboardMedecin = () => {
   const chartRef = useRef(null);
   const chartInstance = useRef(null);
@@ -57,43 +57,7 @@ const DashboardMedecin = () => {
   return (
     <div className="min-h-screen flex font-[Poppins] bg-gradient-to-br from-gray-100 to-gray-50">
       {/* Sidebar */}
-      <aside id="sidebar" className={`fixed top-4 bottom-4  left-4 glass-sidebar flex flex-col justify-between py-4 transition-all duration-300 z-50 ${sidebarOpen ? 'w-60 px-4' : 'w-20 items-center'}`}>
-      <button onClick={toggleSidebar} className="absolute -right-3 top-20 bg-[#9ac441] text-white rounded-full p-1 shadow-lg">
-        {sidebarOpen ? <ChevronLeft /> : <ChevronRight />}
-      </button>
-
-
-        <div className="flex items-center justify-center space-x-2 mb-6">
-          <span className="text-white text-xl font-bold">AI4CKD</span>
-        </div>
-
-        <nav className="space-y-8 flex flex-col text-white text-base mb-6">
-          <a href="#" className="nav-link flex items-center space-x-3">
-            <Home className="sidebar-icon" />
-            <span className={`${sidebarOpen ? 'inline' : 'hidden'}`}>Accueil</span>
-          </a>
-          <a href="#" className="nav-link flex items-center space-x-3">
-            <Users className="sidebar-icon" />
-            <span className={`${sidebarOpen ? 'inline' : 'hidden'}`}>Patients</span>
-          </a>
-          <a href="#" className="nav-link flex items-center space-x-3">
-            <BarChart className="sidebar-icon" />
-            <span className={`${sidebarOpen ? 'inline' : 'hidden'}`}>Suivi</span>
-          </a>
-          <a href="#" className="nav-link flex items-center space-x-3">
-            <ClipboardList className="sidebar-icon" />
-            <span className={`${sidebarOpen ? 'inline' : 'hidden'}`}>Rapports</span>
-          </a>
-        </nav>
-
-        <div className="flex flex-col space-y-6 text-white text-base">
-          <div className="flex items-center space-x-3">
-            <LogOut className="sidebar-icon" />
-            <span className={`${sidebarOpen ? 'inline' : 'hidden'}`}>Déconnexion</span>
-          </div>
-        </div>
-      </aside>
-
+      <SiderbarMedical></SiderbarMedical>
       {/* Main Content */}
       <main className="flex-1 ml-20 md:ml-60 p-6 space-y-8 transition-all duration-300">
         <div className="flex justify-between items-center">
